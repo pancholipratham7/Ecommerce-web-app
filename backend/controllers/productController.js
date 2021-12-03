575738b970edfa4c37f0c1f571ccd213be018ba3
Pratham Pancholi <pancholipratham33@gmail.com>
2021-12-03 20:16:20 +0530

    Fetching the data from the database

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,33 +1,51 @@
-const products = require("./../data/products.js");
+// this package is helpful to catch all the errors in async route handlers and then pass it to your express error handler
+const asyncHandler = require("express-async-handler");
+const Product = require("./../models/productModel");
 
 // get all the products
-exports.getAllProducts = (req, res, next) => {
+exports.getAllProducts = asyncHandler(async (req, res, next) => {
+  // getting all the products from the database
+  const products = await Product.find({});
+
   res.status(200).json({
     status: "success",
     products,
   });
-};
+});
 
 // get a single product
-exports.getProduct = (req, res, next) => {
-  const product = products.find((product) => product._id === req.params.id);
+exports.getProduct = asyncHandler(async (req, res, next) => {
+  // gettting the the particular product from the db through id
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.status(200).json({
+      status: "success",
+      product,
+    });
+  } else {
+    res.status(404).json({
+      status: "failed",
+      message: "Product not find",
+    });
+  }
+});
+
+exports.updateProduct = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     status: "success",
-    product,
   });
-};
-exports.updateProduct = (req, res, next) => {
+});
+
+exports.deleteProduct = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     status: "success",
   });
-};
-exports.deleteProduct = (req, res, next) => {
+});
+
+exports.createNewProduct = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     status: "success",
   });
-};
-exports.createNewProduct = (req, res, next) => {
-  res.status(200).json({
-    status: "success",
-  });
-};
+});
